cb3c9b77e7af203390581057113f2dd99202b976
Anchal2003 <sahuanchal22@gmail.com>
2025-02-23 23:24:26 +0530

    [Anchal Sahu] Add UC2 - Extend Greeting controller and use Service layer to get simple greeting.

--- WelcomeToSpring.java
+++
@@ -1,7 +0,0 @@
-//Create a WelcomeToSpringREST-API to print the welcome statement
-public class WelcomeToSpring {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Spring REST-API\"");
-    }
-
-}
